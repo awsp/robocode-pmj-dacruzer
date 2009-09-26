5cfcb7886ae5d74ca5cca0f18f8956b931710103
philipmjohnson <philipmjohnson@e99b6220-a478-11de-8ba5-91705c1537ed>
2009-09-26 01:27:07 +0000

    Improved testing.

--- src/pmj/DaCruzer.java
+++ src/pmj/DaCruzer.java
@@ -7,47 +7,64 @@ import robocode.ScannedRobotEvent;
  * Implements the DaCruzer robot, which tries to stay away from other robots while firing on 
  * any it finds. 
  * 
- * Source code adapted with gratitude from B.J. DeLaCruz, for which this robot is named. 
+ * 
  * @author Philip Johnson
  */
 public class DaCruzer extends Robot {
-  /**
-   * Distance between the enemy and the robot.
-   */
-  private int distance;
 
   /**
-   * Creates a robot that, at each time, will find the closest enemy and then move in the opposite
-   * direction by one hundred pixels.
+   * Moves to successive corners. Code courtesy of Kimberly Heu. 
    */
   public void run() {
-    distance = Integer.MAX_VALUE;
+    
+    // Get the field's dimensions.
+    double fwidth = getBattleFieldWidth();
+    double fheight = getBattleFieldHeight();
+    
+    // Orient the robot so that it is facing upward.
+    turnLeft(getHeading());
+    
+    // Get the robot's current location.
+    double rposx = getX();
+    double rposy = getY();
+    
+    // Move to top left corner
+    ahead(fheight - rposy);
+    turnLeft(90);
+    ahead(rposx); 
+    turnRight(360);
+    
+    // Move to bottom right corner
+    turnLeft(180);
+    ahead(fwidth);
+    turnRight(90);
+    ahead(fheight);
+    turnRight(360);
+    
+    // Move to top right corner
+    turnLeft(180);
+    ahead(fheight);
+    turnRight(360);
+    
+    // Move to bottom left corner
+    turnLeft(180);
+    ahead(fheight);
+    turnRight(90);
+    ahead(fwidth);
+    turnRight(360);
+    
+    // if finished traversing, scan for robots and fire.
     while (true) {
-      turnRadarRight(10.0);
+      this.turnLeft(90);
     }
   }
 
   /**
-   * Makes the robot turn towards the enemy and move in the opposite direction by one hundred
-   * pixels. If no other enemy is closer, then the robot will remain stationary for the remainder of
-   * the battle. Then fires at the robot.
+   * Fires at any robots it finds. 
    * 
    * @param e contains information about the enemy robot, e.g. its location
    */
   public void onScannedRobot(ScannedRobotEvent e) {
-
-    // getDistance() returns the distance between one robot's center to another robot's center. A
-    // robot is 40 x 40 pixels by default, so get the distance between the two robots' edges. Note
-    // that this is an approximation, so give or take a few pixels.
-    int enemyDistance = (int) Math.floor(e.getDistance() - 40.0);
-
-    if (enemyDistance < distance) {
-      distance = enemyDistance;
-      // Once radar contact is made, turn towards the enemy and then move back one hundred pixels.
-      turnRight(e.getBearing());
-      back(100.0);
-      distance += 100;
-    }
     fire(3);
 
   }
