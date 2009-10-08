b7d5f024d0b910f086618fcc407eb44c3e01daa9
philipmjohnson <philipmjohnson@gmail.com>
2009-10-08 00:51:09 +0000

    Fixing.

---
+++ src/pmj/TestDaCruzerMovement.java
@@ -0,0 +1,80 @@
+package pmj;
+
+import static org.junit.Assert.assertTrue;
+import pmj.test.RobotTestBed;
+import robocode.control.events.BattleCompletedEvent;
+import robocode.control.events.TurnEndedEvent;
+import robocode.control.snapshot.IRobotSnapshot;
+
+/**
+ * Tests that DaCruzer moves to all four corners per its specified strategy.
+ * @author Philip Johnson
+ *
+ */
+public class TestDaCruzerMovement extends RobotTestBed {
+  /** True if the robot visited this corner during the test case. */
+  boolean visitedUpperLeft = false;
+  /** True if the robot visited this corner during the test case. */
+  boolean visitedUpperRight = false;
+  /** True if the robot visited this corner during the test case. */
+  boolean visitedLowerLeft = false;
+  /** True if the robot visited this corner during the test case. */
+  boolean visitedLowerRight = false;
+  
+  /**
+   * Specifies that SittingDuck and DaCruzer are to be matched up in this test case.
+   * @return The comma-delimited list of robots in this match.
+   */
+  @Override
+  public String getRobotNames() {
+    return "sample.SittingDuck,pmj.DaCruzer";
+  }
+  
+  /**
+   * This test runs for 1 round.
+   * @return The number of rounds. 
+   */
+  @Override
+  public int getNumRounds() {
+    return 10;
+  }
+  
+  /**
+   * After each turn, check to see if we're at a corner.  If so, set the corresponding flag.
+   * @param event Info about the current state of the battle.
+   */
+  @Override 
+  public void onTurnEnded(TurnEndedEvent event) {
+    IRobotSnapshot robot = event.getTurnSnapshot().getRobots()[1];
+    double xPos = robot.getX();
+    double yPos = robot.getY();
+    if ((xPos < 40) && (yPos < 40)) {
+      visitedUpperLeft = true;
+    }
+    if ((xPos < 40 && (yPos > (height - 40)))) {
+      visitedLowerLeft = true;
+    }
+    if ((xPos > (width - 40)) && (yPos < 40)) {
+      visitedUpperRight = true;
+    }
+    if ((xPos > (width - 40) && (yPos > (height - 40)))) {
+      visitedLowerRight = true;
+    }
+
+  }
+  
+  
+  /**
+   * After the battle, check to see that we've visited the corners. 
+   * @param event Details about the completed battle.
+   */
+  @Override
+  public void onBattleCompleted(BattleCompletedEvent event) {
+    assertTrue("Check UpperLeft", visitedUpperLeft);
+    assertTrue("Check LowerLeft", visitedLowerLeft);
+    assertTrue("Check UpperRight", visitedUpperRight);
+    assertTrue("Check LowerRight", visitedLowerRight);
+    
+  }
+
+}
---
+++ src/pmj/TestDaCruzerVersusSittingDuck.java
@@ -0,0 +1,48 @@
+package pmj;
+
+import static org.junit.Assert.assertEquals;
+import pmj.test.RobotTestBed;
+import robocode.BattleResults;
+import robocode.control.events.BattleCompletedEvent;
+
+/**
+ * Illustrates JUnit testing of Robocode robots.
+ * This test simply verifies that DaCruzer always beats SittingDuck.
+ * @author Philip Johnson
+ *
+ */
+public class TestDaCruzerVersusSittingDuck extends RobotTestBed {
+
+  /**
+   * Specifies that SittingDuck and DaCruzer are to be matched up in this test case.
+   * @return The comma-delimited list of robots in this match.
+   */
+  @Override
+  public String getRobotNames() {
+    return "sample.SittingDuck,pmj.DaCruzer";
+  }
+  
+  /**
+   * This test runs for 10 rounds.
+   * @return The number of rounds. 
+   */
+  @Override
+  public int getNumRounds() {
+    return 10;
+  }
+  
+  /**
+   * The actual test, which asserts that DaCruzer has won every round against SittingDuck.
+   * @param event Details about the completed battle.
+   */
+  @Override
+  public void onBattleCompleted(BattleCompletedEvent event) {
+    // Return the results in order of getRobotNames.
+    BattleResults[] results = event.getIndexedResults();
+    // Sanity check that results[1] is DaCruzer (not strictly necessary, but illustrative).
+    assertEquals("Check results[1]", results[1].getTeamLeaderName(), "pmj.DaCruzer");
+    
+    // Check to make sure DaCruzer won every round.
+    assertEquals("Check DaCruzer winner", results[1].getFirsts(), getNumRounds());
+  }
+}
