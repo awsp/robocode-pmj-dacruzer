918d46cdb4c91a9987f97ee9d268be223714cee3
edtmeyer <edtmeyer@e99b6220-a478-11de-8ba5-91705c1537ed>
2009-10-08 00:37:16 +0000

    Applied the ICS-SE XML format document to the DaCruzer.java, and both test files in Eclipse to ensure that line lengths, spaces, and JavaDocs conform to our class' coding standards.

--- trunk/src/pmj/TestDaCruzerVersusSittingDuck.java
+++ trunk/src/pmj/TestDaCruzerVersusSittingDuck.java
@@ -6,33 +6,37 @@ import robocode.BattleResults;
 import robocode.control.events.BattleCompletedEvent;
 
 /**
- * Illustrates JUnit testing of Robocode robots.
- * This test simply verifies that DaCruzer always beats SittingDuck.
+ * Illustrates JUnit testing of Robocode robots. This test simply verifies that DaCruzer always
+ * beats SittingDuck.
+ * 
  * @author Philip Johnson
- *
+ * 
  */
 public class TestDaCruzerVersusSittingDuck extends RobotTestBed {
 
   /**
    * Specifies that SittingDuck and DaCruzer are to be matched up in this test case.
+   * 
    * @return The comma-delimited list of robots in this match.
    */
   @Override
   public String getRobotNames() {
     return "sample.SittingDuck,pmj.DaCruzer";
   }
-  
+
   /**
    * This test runs for 20 rounds.
-   * @return The number of rounds. 
+   * 
+   * @return The number of rounds.
    */
   @Override
   public int getNumRounds() {
     return 20;
   }
-  
+
   /**
    * The actual test, which asserts that DaCruzer has won every round against SittingDuck.
+   * 
    * @param event Details about the completed battle.
    */
   @Override
@@ -41,7 +45,7 @@ public class TestDaCruzerVersusSittingDuck extends RobotTestBed {
     BattleResults[] results = event.getIndexedResults();
     // Sanity check that results[1] is DaCruzer (not strictly necessary, but illustrative).
     assertEquals("Check results[1]", results[1].getTeamLeaderName(), "pmj.DaCruzer");
-    
+
     // Check to make sure DaCruzer won every round.
     assertEquals("Check DaCruzer winner", results[1].getFirsts(), getNumRounds());
   }
