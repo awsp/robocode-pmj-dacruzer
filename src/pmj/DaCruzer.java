8077fe46279a33808c7bce1690e8271ba256edde
Bret.K.Ikehara <Bret.K.Ikehara@e99b6220-a478-11de-8ba5-91705c1537ed>
2010-10-12 02:44:31 +0000

    In the onScannedRobot method, the calculations for the firepower and shooting have been separated into two lines to conform to ICS-SE-Java-3 coding standard.

--- src/pmj/DaCruzer.java
+++ src/pmj/DaCruzer.java
@@ -5,8 +5,8 @@ import robocode.ScannedRobotEvent;
 import java.awt.Color;
 
 /**
- * Implements the DaCruzer robot, which tries to stay away from other robots while firing on 
- * any it finds. 
+ * Implements the DaCruzer robot, which tries to stay away from other robots while firing on any it
+ * finds.
  * 
  * @author Philip Johnson
  * @version 1.2
@@ -14,54 +14,54 @@ import java.awt.Color;
 public class DaCruzer extends Robot {
 
   /**
-   * Moves to successive corners and rotates the robot to maximize the chances of scanning
-   * another robot. Code courtesy of Kimberly Heu. 
+   * Moves to successive corners and rotates the robot to maximize the chances of scanning another
+   * robot. Code courtesy of Kimberly Heu.
    */
   public void run() {
-    
+
     // Get the field's dimensions.
-    //Specifies battle field width
+    // Specifies battle field width
     double fWidth = getBattleFieldWidth();
-    //Specifies battle field Height
-    double fHeight = getBattleFieldHeight(); 
-    
+    // Specifies battle field Height
+    double fHeight = getBattleFieldHeight();
+
     // Orient the robot so that it is facing upward.
     turnLeft(getHeading());
-    
-    //change bullet color to red.
+
+    // change bullet color to red.
     setBulletColor(Color.red);
-    //change radar color to black.
+    // change radar color to black.
     setRadarColor(Color.black);
-      
+
     // Get the robot's current location.
     double rposX = getX();
     double rposY = getY();
-    
+
     // Move to top left corner
     ahead(fHeight - rposY);
     turnLeft(90);
-    ahead(rposX); 
+    ahead(rposX);
     turnRight(360);
-    
+
     // Move to bottom right corner
     turnLeft(180);
     ahead(fWidth);
     turnRight(90);
     ahead(fHeight);
     turnRight(360);
-    
+
     // Move to top right corner
     turnLeft(180);
     ahead(fHeight);
     turnRight(360);
-    
+
     // Move to bottom left corner
     turnLeft(180);
     ahead(fHeight);
     turnRight(90);
     ahead(fWidth);
     turnRight(360);
-    
+
     // if finished traversing, scan for robots and fire.
     // spins the body of the robot to conduct scans.
     while (true) {
@@ -70,11 +70,14 @@ public class DaCruzer extends Robot {
   }
 
   /**
-   * Fires at any robots it finds with a power that is based on distance from target. 
+   * Fires at any robots it finds with a power that is based on distance from target.
    * 
    * @param e contains information about the enemy robot, e.g. its location
    */
   public void onScannedRobot(ScannedRobotEvent e) {
-    fire(Math.min(400 / e.getDistance(), 3));
+
+    double firePower = Math.min(400 / e.getDistance(), 3);
+        
+    this.fire(firePower);
   }
 }
