c7b24fc189235945e2fdf7294c1d69162af89e73
Christopher.T.Foo@gmail.com <Christopher.T.Foo@gmail.com@e99b6220-a478-11de-8ba5-91705c1537ed>
2011-10-13 10:39:20 +0000

    Fixed capitalization of comments.  Added periods to the ends of comments.  Fixed semantics of the run method's while(true) loop comment.

--- trunk/src/pmj/DaCruzer.java
+++ trunk/src/pmj/DaCruzer.java
@@ -21,50 +21,50 @@ public class DaCruzer extends Robot {
   public void run() {
 
     // Get the field's dimensions.
-    // Specifies battle field width
+    // Specifies battle field width.
     double fieldWidth = getBattleFieldWidth();
-    // Specifies battle field Height
+    // Specifies battle field Height.
     double fieldHeight = getBattleFieldHeight();
 
     // Orient the robot so that it is facing upward.
     turnLeft(getHeading());
 
-    // change bullet color to red.
+    // Change bullet color to red.
     setBulletColor(Color.red);
-    // change radar color to black.
+    // Change radar color to black.
     setRadarColor(Color.black);
 
     // Get the robot's current location.
     double robotPositionX = getX();
     double robotPositionY = getY();
 
-    // Move to top left corner
+    // Move to top left corner.
     ahead(fieldHeight - robotPositionY);
     turnLeft(90);
     ahead(robotPositionX);
     turnRight(360);
 
-    // Move to bottom right corner
+    // Move to bottom right corner.
     turnLeft(180);
     ahead(fieldWidth);
     turnRight(90);
     ahead(fieldHeight);
     turnRight(360);
 
-    // Move to top right corner
+    // Move to top right corner.
     turnLeft(180);
     ahead(fieldHeight);
     turnRight(360);
 
-    // Move to bottom left corner
+    // Move to bottom left corner.
     turnLeft(180);
     ahead(fieldHeight);
     turnRight(90);
     ahead(fieldWidth);
     turnRight(360);
 
-    // if finished traversing, scan for robots and fire.
-    // spins the body of the robot 90 degrees to conduct scans.
+    // If finished traversing, scan for robots and fire.
+    // Spins the gun of the robot 90 degrees to conduct scans.
     while (true) {
       this.turnGunLeft(90);
     }
@@ -73,7 +73,7 @@ public class DaCruzer extends Robot {
   /**
    * Fires at any robots it finds with a power that is based on distance from target.
    * 
-   * @param e contains information about the enemy robot, e.g. its location
+   * @param e contains information about the enemy robot, e.g. its location.
    */
   @Override
   public void onScannedRobot(ScannedRobotEvent e) {
@@ -84,7 +84,7 @@ public class DaCruzer extends Robot {
   /**
    * Victory Dance once the robot has won.
    * 
-   * @param win takes place when robot wins
+   * @param win takes place when robot wins.
    */
   @Override
   public void onWin(WinEvent win) {
