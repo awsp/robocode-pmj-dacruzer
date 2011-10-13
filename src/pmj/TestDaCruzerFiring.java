6e565e375d4d22b06ca735d3e89e2c4cd3d68642
J.K.Takayama <J.K.Takayama@gmail.com>
2011-10-13 22:53:09 +0000

    Improved the documentation to provide a more specific description of the firing behavior which this program checks for.

--- src/pmj/TestDaCruzerFiring.java
+++ src/pmj/TestDaCruzerFiring.java
@@ -8,66 +8,60 @@ import robocode.control.testing.RobotTestBed;
 
 /**
  * This tests for variability in bullet power administered by the robot DaCruzer.
- * 
+ * To pass, DaCruzer must fire bullets with power less than 1.4, power in the range
+ * 1.5 to 1.9, power in the range 2.0 to 2.4, and power greater than 2.4.
+ *
  * @author Philip Johnson
  */
 public class TestDaCruzerFiring extends RobotTestBed {
 
-  /**
-   *  True if DaCruzer has fired a bullet with power less than 1.4.
-   */
+  // True if DaCruzer has fired a bullet with power less than 1.4;
   boolean firePowerReachLevelOne = false;
-  /**
-   *  True if DaCruzer has fired a bullet with power between 1.5 to 1.9.
-   */
+  // True if DaCruzer has fired a bullet with power between 1.5 to 1.9;
   boolean firePowerReachLevelTwo = false;
-  /**
-   *  True if DaCruzer has fired a bullet with power between 2.0 to 2.4.
-   */
+  // True if DaCruzer has fired a bullet with power between 2.0 to 2.4;
   boolean firePowerReachLevelThree = false;
-  /**
-   *  True if DaCruzer has fired a bullet with power greater than 2.4.
-   */
+  // True if DaCruzer has fired a bullet with power greater than 2.4;
   boolean firePowerReachLevelFour = false;
-
+  
   /**
    * Specifies that SittingDuck and DaCruzer are to be matched up in this test case.
    * 
    * @return The comma-delimited list of robots in this match.
    */
-  @Override
+  @Override 
   public String getRobotNames() {
     return "sample.Tracker,pmj.DaCruzer";
   }
-
+  
   /**
    * This test runs for 20 rounds.
    * 
-   * @return The number of rounds.
+   * @return The number of rounds. 
    */
-  @Override
+  @Override 
   public int getNumRounds() {
     return 20;
   }
-
+  
   /**
-   * At the end of each turn, checks the power of all bullets moving across the battlefield. Checks
-   * to see if there is any variability in bullet power, proving that DaCruzer is using proportional
-   * firing based on distance of detected enemy robot.
+   * At the end of each turn, checks the power of all bullets moving across the 
+   * battlefield. Checks to see if there is any variability in bullet power, proving
+   * that DaCruzer is using proportional firing based on distance of detected enemy robot. 
    * 
    * @param event Info about the current state of the battle.
    */
-  @Override
-  public void onTurnEnded(TurnEndedEvent event) {
-
+  @Override 
+  public void onTurnEnded (TurnEndedEvent event) {
+    
     // All active bullets belong to DaCruzer since SittingDuck does not fire.
     IBulletSnapshot bullets[] = event.getTurnSnapshot().getBullets();
     double bulletPower;
-
+    
     for (int i = 0; i < bullets.length; i++) {
-
+    
       bulletPower = bullets[i].getPower();
-
+      
       if (bulletPower <= 1.4) {
         firePowerReachLevelOne = true;
       }
@@ -82,13 +76,13 @@ public class TestDaCruzerFiring extends RobotTestBed {
       }
     }
   }
-
+  
   /**
    * After running all matches, determine if DaCruzer has had variability in its bullet power.
    * 
    * @param event Details about the completed battle.
    */
-  @Override
+  @Override 
   public void onBattleCompleted(BattleCompletedEvent event) {
     assertTrue("Bullet Power less than 1.4", firePowerReachLevelOne);
     assertTrue("Bullet Power between 1.5 and 1.9", firePowerReachLevelTwo);
